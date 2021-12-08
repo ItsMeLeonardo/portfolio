029230a3a987cd6eed01e6a521b0db8f603f2893
Carlos Leonardao <74982565+ItsMeLeonardo@users.noreply.github.com>
2021-12-08 12:32:36 -0500

    [DEPLOY] with Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vite'
+//@ts-ignore
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      entry: 'src/my-app.ts',
+      entry: 'src/my-elements.ts',
       formats: ['es'],
     },
     rollupOptions: {
-      external: /^lit/,
+      input: {
+        //@ts-ignore
+        main: resolve(__dirname, 'index.html'),
+      },
     },
   },
 })
